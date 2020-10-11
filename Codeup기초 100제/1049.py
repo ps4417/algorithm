762fa0ce12d27bf1f25498e18751cd7e9d214dd1
ps4417 <ps4417@naver.com>
2020-10-11 09:49:21 +0900

    기조-비교연산

---
+++ Codeup기초 100제/1049.py
@@ -0,0 +1,6 @@
+#  [기초-비교연산] 두 정수 입력받아 비교하기1
+a,b = map(int,input().split())
+if a > b :
+    print(1)
+else:
+    print(0)
---
+++ Codeup기초 100제/1051.py
@@ -0,0 +1,6 @@
+# [기초-비교연산] 두 정수 입력받아 비교하기3
+a,b = map(int,input().split())
+if b>=a:
+    print(1)
+else:
+    print(0)
